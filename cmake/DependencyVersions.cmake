c2e70e9a1fc96a5d6393852f1a9c391d4ad974c4
voyage200 <hello_cmd@outlook.com>
2026-02-21 02:04:10 +0800

    update deps

--- cmake/DependencyVersions.cmake
+++ cmake/DependencyVersions.cmake
@@ -2,11 +2,11 @@
 # Ensures reproducible builds across different environments
 
 set(DEP_LIBYAML_VERSION    "0.2.5"       CACHE STRING "LibYAML version")
-set(DEP_EXPAT_VERSION      "R_2_6_4"     CACHE STRING "Expat version")
+set(DEP_EXPAT_VERSION      "R_2_7_4"     CACHE STRING "Expat version")
 set(DEP_LEVELDB_VERSION    "1.23"        CACHE STRING "LevelDB version")
-set(DEP_ZLIB_VERSION       "v1.3.1.2"    CACHE STRING "Zlib version")
-set(DEP_BROTLI_VERSION     "v1.1.0"      CACHE STRING "Brotli version")
-set(DEP_ASIO_VERSION       "asio-1-30-2" CACHE STRING "ASIO version")
+set(DEP_ZLIB_VERSION       "v1.3.2"      CACHE STRING "Zlib version")
+set(DEP_BROTLI_VERSION     "v1.2.0"      CACHE STRING "Brotli version")
+set(DEP_ASIO_VERSION       "asio-1-36-0" CACHE STRING "ASIO version")
 set(DEP_RAPIDJSON_VERSION  "v1.1.0"      CACHE STRING "RapidJSON version")
 
 set(DEP_LIBYAML_REPO    "https://github.com/yaml/libyaml.git"       CACHE STRING "LibYAML repository")
